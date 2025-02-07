2f8c6b4f988548304f0a78f64a3edb9c4bda1fd1
Jagadeesh Talari <talarijagadeesh55@gmail.com>
2025-02-07 22:27:31 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hi Now you are seeing home page on server side");
+});
+
+app.get("/api/auth", (req, res) => {
+  res.send("Now this is auth route");
+});
+
+app.listen(4000, () => {
+  console.log(`Server is listening on port 4000`);
+});
